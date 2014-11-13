367959d1dc3327e47f7779c0445087a143a9de43
Derek Argueta <darguetap@gmail.com>
2014-11-13 00:43:41 -0700

    updated pie chart stats and standardized university names

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -16,14 +16,14 @@ $(window).stellar();
 //ChartJS
 var schoolData = [
     {
-        value: 17,
+        value: 20,
         color:"#428BCA",
-        label: "Brigham Young University"
+        label: "Brigham Young University--Provo"
     },
     {
         value: 8,
         color: "#555",
-        label: "The University of Illinois, Urbana-Champaign"
+        label: "University of Illinois--Urbana-Champaign"
     },
     {
         value: 5,
@@ -34,17 +34,17 @@ var schoolData = [
         value: 2,
         color:"#909090",
         highlight: "#FF5A5E",
-        label: "The University of Texas, Austin"
+        label: "University of Texas--Austin"
     },
     {
-        value: 9,
+        value: 10,
         color:"#eee",
         label: "Others"
     }
 ];
 
 var pieOptions = {
-  tooltipTemplate: "<%=label%>: <%= Math.round((value/41)*100) + '%' %>",
+  tooltipTemplate: "<%=label%>: <%= Math.round((value/45)*100) + '%' %>",
   onAnimationComplete: function()
   {
       this.showTooltip(this.segments, true);
